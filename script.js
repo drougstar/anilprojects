e9bb4e76e645ca9ef003bd2cf14ac25e427b39e3
drougstar <sizofko@gmail.com>
2021-11-16 22:24:07 +0300

    main

--- script.js
+++ script.js
@@ -3,13 +3,16 @@
 const modalWeek = document.querySelector(".week"),
   modalGuess = document.querySelector(".guess"),
   modalDice = document.querySelector(".dice"),
+  modalColor = document.querySelector(".color"),
   overlay = document.querySelector(".overlay"),
   closeButtonWeek = document.querySelector(".close-button-week"),
   closeButtonGuess = document.querySelector(".close-button-guess"),
   closeButtonDice = document.querySelector(".close-button-dice"),
+  closeButtonColor = document.querySelector(".close-button-color"),
   modalButtonWeek = document.querySelector(".show-modal-week"),
   modalButtonGuess = document.querySelector(".show-modal-guess"),
   modalButtonDice = document.querySelector(".show-modal-dice"),
+  modalButtonColor = document.querySelector(".show-modal-color"),
   pressedKey = "Escape",
   // Adding a function for to close multiple ways with a click
   eLClick = function (functions, keywords, elements) {
@@ -32,6 +35,11 @@ const modalWeek = document.querySelector(".week"),
         closeModalDice();
       }
     });
+    document.addEventListener("keydown", function (e) {
+      if (e.key === keywords && !modalDice.classList.contains("hidden")) {
+        closeModalColor();
+      }
+    });
   },
   // Adding a function for to show hidden elements
   openModalWeek = function () {
@@ -48,6 +56,11 @@ const modalWeek = document.querySelector(".week"),
     modalDice.classList.remove("hidden");
     overlay.classList.remove("hidden");
   },
+  // Adding a function for to show hidden elements
+  openModalColor = function () {
+    modalColor.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  },
   // Adding a function for to hide elements
   closeModalWeek = function () {
     modalWeek.classList.add("hidden");
@@ -62,6 +75,11 @@ const modalWeek = document.querySelector(".week"),
   closeModalDice = function () {
     modalDice.classList.add("hidden");
     overlay.classList.add("hidden");
+  },
+  // Adding a function for to hide elements
+  closeModalColor = function () {
+    modalColor.classList.add("hidden");
+    overlay.classList.add("hidden");
   };
 
 // Listening if button is clicked and showing according to that
@@ -69,7 +87,9 @@ const modalWeek = document.querySelector(".week"),
 eLClick(openModalWeek, "", modalButtonWeek);
 eLClick(openModalGuess, "", modalButtonGuess);
 eLClick(openModalDice, "", modalButtonDice);
+eLClick(openModalColor, "", modalButtonColor);
 
 eLClick(closeModalWeek, pressedKey, closeButtonWeek, overlay);
 eLClick(closeModalGuess, pressedKey, closeButtonGuess, overlay);
 eLClick(closeModalDice, pressedKey, closeButtonDice, overlay);
+eLClick(closeModalColor, pressedKey, closeButtonColor, overlay);
